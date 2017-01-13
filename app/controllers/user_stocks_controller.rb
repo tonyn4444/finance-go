0a20dc3eddc76298bd2ae599935dc584e5c584ce
Tony Nguyen <tntuyen@ucdavis.edu>
2017-01-12 16:41:48 -0800

    add list of user_stocks, add option to remove stock from user portfolio

--- app/controllers/user_stocks_controller.rb
+++ app/controllers/user_stocks_controller.rb
@@ -43,7 +43,7 @@ class UserStocksController < ApplicationController
 
     respond_to do |format|
       if @user_stock.save
-        format.html { redirect_to my_portfolio_path, notice: "Stock #{@user_stock.stock.ticker} stock was successfully added"
+        format.html { redirect_to my_portfolio_path, notice: "#{@user_stock.stock.ticker} stock was successfully added"
    }
         format.json { render :show, status: :created, location: @user_stock }
       else
@@ -72,7 +72,7 @@ class UserStocksController < ApplicationController
   def destroy
     @user_stock.destroy
     respond_to do |format|
-      format.html { redirect_to user_stocks_url, notice: 'User stock was successfully destroyed.' }
+      format.html { redirect_to my_portfolio_path, notice: 'Stock was successfully removed from your portfolio.' }
       format.json { head :no_content }
     end
   end
